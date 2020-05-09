c1fe63f983904325b7b26edcb79933c5c2ee8dd0
Alberto Garcia de la Faya <albertodelafaya@gmail.com>
2020-05-09 21:18:46 +0200

    deleted previous pictograms

--- public/pictograms/salud/acentos.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for f in *; do
-  nuevo=$(echo "$f" |   sed  'y/áÁàÀãÃâÂéÉêÊíÍóÓõÕôÔúÚçÇªº/aAaAaAaAeEeEiIoOoOoOuUcCao/')
-  test  "$f" != "$nuevo" &&  (echo "Corrigiendo $f" ; mv "$f" "$nuevo")
-done
